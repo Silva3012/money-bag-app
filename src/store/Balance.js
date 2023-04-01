3943e78441c25197cd2ed97ec13a52335d211238
Silva3012 <silvano.n92@gmail.com>
2023-04-01 21:47:44 +0200

    Created Balance component to show current balance and handle user input

--- src/store/Balance.js
+++ src/store/Balance.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 //Gomponent that retrieves the current balance from the Redux store and display it.
@@ -6,9 +6,47 @@ const Balance = () => {
     // Get the current balance from the Redux store using the useSelector hook
     const balance = useSelector(state => state.balance);
 
+    // State for input field and set default value to empty string
+    const [inputAmount, setInputAmount] = useState("");
+
     //Function to handle input change in input field
     const handleInputChange = (e) => {
         setInputAmount(e.target.value);
+    }
+
+    //Function to handle deposit button click
+    const handleDeposit = () => {
+        if (inputAmount) {
+            const depositAmount = parseFloat(inputAmount);
+            if (!isNaN(depositAmount) && depositAmount > 0) {
+                //dispatch deposit action with deposit amount
+                //code for dispatching deposit goes here
+                setInputAmount("");
+            }
+        }
+    }
+
+    //Function to handle withdrawal button click
+    const handleWithdrawal = () => {
+        if (inputAmount) {
+            const withdrawalAmount = parseFloat(inputAmount);
+            if (!isNaN(withdrawalAmount) && withdrawalAmount > 0) {
+                // dispatch withdrawal action with withdrawal amount
+                // code for dispatching withdrawal action goes here
+                setInputAmount("");
+            }
+        }
+    }
+
+    //Function to handle interest button click
+    const handleInterest = () => {
+        // dispatch interest action
+        // code for dispatching interest action goes here
+    }
+
+    const handleCharge = () => {
+        // dispatch charge action
+        // code for dispatching charge action goes here
     }
 
     return (
